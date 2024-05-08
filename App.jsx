3170ac3f5d4c1302e7ae2d80d251e59aaf88d094
Francisco Sabino <fdasabino@gmail.com>
2024-05-08 10:30:06 +0200

    chore: Update app name and version in app.json

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, Text } from "react-native";
+
+export default function App() {
+    return (
+        <SafeAreaView>
+            <Text>Our React native blank canvas</Text>
+        </SafeAreaView>
+    );
+}
