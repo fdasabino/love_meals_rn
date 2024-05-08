88b28e0182ff3538df3852c6b5fc075509079314
Francisco Sabino <fdasabino@gmail.com>
2024-05-08 20:10:48 +0200

    refactor: Update restaurant screen to use FlatList for improved performance
    The RestaurantsScreen component has been updated to use the FlatList component from react-native for rendering the list of restaurants. This change improves the performance of the screen by implementing lazy loading for images and optimizing the rendering of the restaurant cards.

--- src/components/features/restaurants/screens/restaurants.screen.styles.js
+++ src/components/features/restaurants/screens/restaurants.screen.styles.js
@@ -11,6 +11,10 @@ export const Search = styled.View`
 
 export const SearchBar = styled(Searchbar)`
     padding-right: ${(props) => props.theme.space[2]};
+    background-color: ${(props) => props.theme.colors.brand.muted};
+    border-radius: 10px;
+    border-width: 2px;
+    border-color: ${(props) => props.theme.colors.brand.primary};
 `;
 
 export const RestaurantsList = styled(FlatList).attrs({
