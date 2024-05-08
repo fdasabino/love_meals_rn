386ccde3034eb84a168d541d35397f85948f5d6a
Francisco Sabino <fdasabino@gmail.com>
2024-05-08 13:52:25 +0200

    refactor: Update app.json and package.json for font and asset changes
    This commit updates the app.json file to include the assets directory for fonts and modifies the package.json file to add new dependencies for Google Fonts and Expo App Loading. These changes improve the app's typography and ensure proper loading of fonts and assets.

--- App.jsx
+++ App.jsx
@@ -1,12 +1,24 @@
 // Imports
+import { Lato_400Regular } from "@expo-google-fonts/lato";
+import { Oswald_400Regular, useFonts } from "@expo-google-fonts/oswald";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infrastructure/theme/index";
 
 // Screens
+import AppLoading from "expo-app-loading";
 import RestaurantsScreen from "./src/components/features/restaurants/screens/restaurants.screen";
 
 const App = () => {
+    const [fontsLoaded] = useFonts({
+        Oswald_400Regular,
+        Lato_400Regular,
+    });
+
+    if (!fontsLoaded) {
+        <AppLoading />;
+    }
+
     return (
         <>
             <ThemeProvider theme={theme}>
--- src/components/features/restaurants/components/styled.js
+++ src/components/features/restaurants/components/styled.js
@@ -9,6 +9,8 @@ export const StyledCard = styled(Card)`
 export const Title = styled.Text`
     padding: ${(props) => props.theme.space[3]};
     color: ${(props) => props.theme.colors.ui.success};
+    font-family: ${(props) => props.theme.fonts.heading};
+    font-size: ${(props) => props.theme.fontSizes.title};
 `;
 
 export const CardCover = styled(Card.Cover)`
